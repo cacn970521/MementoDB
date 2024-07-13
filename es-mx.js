95d0d36042ab73059f5899c0008c3e31c269d51f
Noé Eduardo Capetillo Cano <71462203+cacn970521@users.noreply.github.com>
2024-07-13 07:55:15 -0600

    Update es-mx.js

--- es-mx.js
+++ es-mx.js
@@ -65,8 +65,7 @@
             L: 'DD/MM/YYYY',
             LL: 'D [de] MMMM [de] YYYY',
             LLL: 'D [de] MMMM [de] YYYY H:mm',
-            LLLL: 'dddd, D [de] MMMM [de] YYYY H:mm',
-            LLLS: 'dddd, DD [de] MMMM',
+            LLLL: 'dddd, DD [de] MMMM',
         },
         calendar: {
             sameDay: function () {
