2cad8a6f9a297deb636246439961d94e54b6488c
Noé Eduardo Capetillo Cano <71462203+cacn970521@users.noreply.github.com>
2024-07-13 07:59:59 -0600

    Update moment.js

--- moment.js
+++ moment.js
@@ -557,7 +557,7 @@
         L: 'MM/DD/YYYY',
         LL: 'MMMM D, YYYY',
         LLL: 'MMMM D, YYYY h:mm A',
-        LLLL: 'dddd, MMMM D, YYYY h:mm A',
+        LLLL: 'dddd, DD MMMM',
     };
 
     function longDateFormat(key) {
